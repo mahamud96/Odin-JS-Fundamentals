44fa05eaf13f321e86041a99d839962607b641b4
Mahamud Ahmed <mahamud1996@outlook.com>
2022-05-07 14:55:06 +0100

    move JS to sepaerate file, add numbers to js file

---
+++ script.js
@@ -0,0 +1,67 @@
+let name = 'John'
+let admin = name
+
+
+
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+let a = 6
+let b = 5
+let c = a+b
+
+let six = 6
+let five = 5
+let one = six-five
+
+
+let x = 5;
+x++;
+let y = x;
+
+console.log (x *= y)
+
+let l = 5;
+l--;
+let m = l;
+
+let p = 3;
+let q = p ** 3;
+let r = Math.pow(p,5)
+
+
+console.log (r)
+
+const myInt = 5;
+const myFloat = 6.667 
+
+
+console.log (10 + 7)
+console.log (9 * 8)
+console.log (60 % 3)
+
+let num1 = 10;
+let num2 = 40;
+
+console.log (9 * num1)
+console.log (num1 ** 3)
+console.log (num2 / num1)
+
+console.log (5 + 10 * 3)
+console.log (num2 % 9 * num1)
+console.log (num2 + num1 / 8 + 2)
+console.log ((num2 + num1)/(8+2))
+console.log (++num1)
+console.log (num1)
+console.log (num2--)
+console.log (num2)
+console.log (num1<=num2)
+
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max
+
+console.log (percentage)
+
+myInt
+myFloat
